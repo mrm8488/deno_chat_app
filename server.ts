fa4b0719c554dc9f69e48dd0340693f5782da937
mrm8488 <mrm8488@gmail.com>
2020-05-20 14:30:55 +0200

    First commit

---
+++ server.ts
@@ -0,0 +1,31 @@
+import { listenAndServe } from "https://deno.land/std/http/server.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+import { chat } from "./chat.ts";
+
+listenAndServe({ port: 3000 }, async (req) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/") {
+    req.respond({
+      status: 200,
+      headers: new Headers({
+        "content-type": "text/html",
+      }),
+      body: await Deno.open("./index.html"),
+    });
+  }
+
+  // WebSockets Chat
+  if (method === "GET" && url === "/ws") {
+    if (acceptable(req)) {
+      acceptWebSocket({
+        conn: req.conn,
+        bufReader: req.r,
+        bufWriter: req.w,
+        headers: req.headers,
+      }).then(chat);
+    }
+  }
+});
+
+console.log("Server running on localhost:3000");
